6191292e9e6eb293ef5cb9238184c5959eba8d72
Uian Sol <sol.uian@gmail.com>
2014-06-20 07:16:02 -0300

    Dynamic params to configure stitcher included.

--- src/stitcher/src/main.cpp
+++ src/stitcher/src/main.cpp
@@ -13,10 +13,14 @@
 namespace enc = sensor_msgs::image_encodings;
 static const char WINDOW[] = "Image Processed";
 image_transport::Publisher pub;
-int threshold_value = 0;
-int threshold_type = 3;
-bool can_stitch;
+std::vector <cv::Mat> vImg;
 
+int stitcher_num_frames = 2;
+double registr_resol = 0.6;
+double seam_est_resol = 0.1;
+double compose_resol = -1;
+double conf_thresh = 1;
+bool do_wave_correct = true;
 
 void stitcherCallback(const sensor_msgs::ImageConstPtr& original_image) {
     cv_bridge::CvImagePtr cv_ptr;
@@ -27,17 +31,9 @@ void stitcherCallback(const sensor_msgs::ImageConstPtr& original_image) {
         return;
     }
 
-    if (!can_stitch) {
-        cv::imwrite("images/Image1.jpg", cv_ptr->image);
-        can_stitch = true;
-        sleep(2);
-    } else {
-        std::vector <cv::Mat> vImg;
-        cv::Mat rImg;
-
-        vImg.push_back(cv::imread("images/Image1.jpg"));
+    if (vImg.size() == stitcher_num_frames - 1) { 
         vImg.push_back(cv_ptr->image);
-
+        
         cv::Stitcher stitcher = cv::Stitcher::createDefault();
         stitcher.stitch(vImg, cv_ptr->image);
         
@@ -45,39 +41,31 @@ void stitcherCallback(const sensor_msgs::ImageConstPtr& original_image) {
         cv::imshow(WINDOW, cv_ptr->image);
         cv::waitKey(3);
         pub.publish(cv_ptr->toImageMsg());
-        can_stitch = false;
         sleep(2);
+        vImg.clear();
+    } else {
+        vImg.push_back(cv_ptr->image);
     }
 }
-/*
-void imageCallback(const sensor_msgs::ImageConstPtr& original_image) {
-    cv_bridge::CvImagePtr cv_ptr;
-    try {
-        cv_ptr = cv_bridge::toCvCopy(original_image, enc::BGR8);
-    } catch (cv_bridge::Exception& e) {
-        ROS_ERROR("tutorialROSOpenCV::main.cpp::cv_bridge exception: %s", e.what());
-        return;
-    }
 
-    threshold(cv_ptr->image, cv_ptr->image, threshold_value, 255, threshold_type);
-
-    cv::imshow(WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-    pub.publish(cv_ptr->toImageMsg());
-}
-*/
 void configCallBack(stitcher::StitcherConfig &config, uint32_t level) {
-    threshold_value = config.threshold_value;
-    threshold_type = config.threshold_type;
-    
-    ROS_INFO("Reconfigure Request: %d %d",
-                config.threshold_value,
-                config.threshold_type);
+    stitcher_num_frames = config.stitcher_num_frames;
+    registr_resol = config.registr_resol;
+    seam_est_resol = config.seam_est_resol;
+    compose_resol = config.compose_resol;
+    conf_thresh = config.conf_thresh;
+    do_wave_correct = config.do_wave_correct;
+
+    ROS_INFO("Reconfigure Request: %d %f %f %f %f %d",
+                stitcher_num_frames,
+                registr_resol,
+                seam_est_resol,
+                compose_resol,
+                conf_thresh,
+                do_wave_correct);
 }
  
 int main(int argc, char **argv) {
-    can_stitch = false;
-    
     ros::init(argc, argv, "image_processor");
     ros::NodeHandle nh;
     
